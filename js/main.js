2fd1b0e1caaeeee78ba3e8728b6e830e4e353c7a
Stephan Detje <stephan.detje@sap.com>
2015-10-27 04:11:15 +0000

    added header and backend call

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 function init() {
-    
+	
 }
 
 $(function(){
     init();
 });
+
+
+function fetchData(){
+	$.ajax("https://s10hanaxs.hanatrial.ondemand.com/i848741trial/hanatrial/innojam/innojam_xs_backend/db_test.xsjs")
+		.done(function(data){
+			console.log(data);
+		});
+}
+
+fetchData();
