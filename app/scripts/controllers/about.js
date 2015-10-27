667993eb617a1e24495d339e27ed1448a348e153
Michael Janke <michael.jank@student.hpi.uni-potsdam.de>
2015-10-26 23:12:26 -0700

    generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name matchUpApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the matchUpApp
+ */
+angular.module('matchUpApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name matchUpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the matchUpApp
+ */
+angular.module('matchUpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
